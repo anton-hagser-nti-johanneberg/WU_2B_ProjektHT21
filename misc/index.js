27527c452ca6469cdd89644b364999c7b10125c1
Anton Hagsér <mrfullofrats@gmail.com>
2022-01-05 20:19:08 +0100

    added landing page and some initial styling

--- misc/index.js
+++ misc/index.js
@@ -3,10 +3,10 @@ mapboxgl.accessToken =
 const map = new mapboxgl.Map({
     container: "map", // container ID
     style: "mapbox://styles/mapbox/streets-v11", // style URL
-    center: [11.9743768, 57.6905535], // starting position
-    pitch: 45,
-    bearing: -17.6,
-    zoom: 16, // starting zoom
+    center: [14.208328809133434, 57.65571605834185], // starting position
+    pitch: 1,
+    bearing: -11.56404140660961,
+    zoom: 5.1065697131145065, // starting zoom
 });
 
 map.on("load", () => {
@@ -22,7 +22,7 @@ map.on("load", () => {
             "source-layer": "building",
             filter: ["==", "extrude", "true"],
             type: "fill-extrusion",
-            minzoom: 9,
+            minzoom: 12,
             paint: {
                 "fill-extrusion-color": "#aaa",
                 "fill-extrusion-height": [
@@ -48,7 +48,38 @@ map.on("load", () => {
         },
         labelLayerId
     );
+
+    const gothenburg = new mapboxgl.Marker({
+        color: "red",
+        draggable: true,
+    })
+        .setLngLat([11.9711353, 57.7011121])
+        .addTo(map);
+
+    const halmstad = new mapboxgl.Marker({
+        color: "red",
+        draggable: true,
+    })
+        .setLngLat([12.8567559, 56.6752514])
+        .addTo(map);
+
+    const stockholm = new mapboxgl.Marker({
+        color: "red",
+        draggable: true,
+    })
+        .setLngLat([18.083367, 59.3347249])
+        .addTo(map);
 });
 
 // Add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl());
+
+// Handle sticky navbar
+var navbar = document.getElementById("navbar");
+window.onscroll = function () {
+    if (window.pageYOffset >= 50) {
+        navbar.classList.add("navbar-small");
+    } else {
+        navbar.classList.remove("navbar-small");
+    }
+};
